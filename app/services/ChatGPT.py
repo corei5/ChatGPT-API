fbde4658b699987a0c07752f17ed1491d1f37522
gollam rabby <rabby2186@gmail.com>
2023-01-11 04:35:18 +0600

    Update service file

--- app/services/ChatGPT.py
+++ app/services/ChatGPT.py
@@ -13,14 +13,18 @@ class ChatGpt_Class:
         super().__init__()
 
     def ChatGpt_login(self):
-        session_token = ""
+        session_token = ''
         api = ChatGPT(session_token)  # auth with session token
         return api
 
     def ChatGpt_output(self,text):
-        api = self.ChatGpt_login()
         try:
-            resp = api.send_message(text)
-            return resp['message']
+            api = self.ChatGpt_login()
+            try:
+                resp = api.send_message(text)
+                return resp['message']
+            except:
+                return "Getting error from ChatGpt"
         except:
-            return "Getting error from ChatGpt"
+            return "Not able to login in ChatGpt"
+
