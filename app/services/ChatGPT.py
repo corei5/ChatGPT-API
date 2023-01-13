f708bfb30158d35701c24b9991ec37fd21dec9ca
gollam rabby <rabby2186@gmail.com>
2023-01-13 21:37:52 +0600

    update language

--- app/services/ChatGPT.py
+++ app/services/ChatGPT.py
@@ -1,6 +1,9 @@
 import logging
 from pyChatGPT import ChatGPT
 
+from googletrans import Translator
+translator = Translator()
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -17,12 +20,16 @@ class ChatGpt_Class:
         api = ChatGPT(session_token)  # auth with session token
         return api
 
-    def ChatGpt_output(self,text):
+    def ChatGpt_output(self,text, language):
         try:
             api = self.ChatGpt_login()
             try:
-                resp = api.send_message(text)
-                return resp['message']
+                text_conv_eng = translator.translate(text, dest='en')
+                print(text_conv_eng.text)
+                resp = api.send_message(text_conv_eng.text)
+                print(language)
+                message = translator.translate(resp['message'], dest=str(language))
+                return message.text
             except Exception as e:
                 print(e)
                 return "Getting error from ChatGpt"
